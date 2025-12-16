5ae5a6a15d7429106e566b4ad5c1cea83e1b6514
Albin H <albin.heldebro@outlook.com>
2025-12-16 19:51:03 +0100

    Visualization of data and prediction

--- NASAforest.py
+++ NASAforest.py
@@ -4,10 +4,11 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
 import numpy as np
+import matplotlib.pyplot as plt
 
 filepath = "archive/CMaps/train_FD001.txt"
 
-df = pd.read_csv(filepath, delim_whitespace=True, header=None)
+df = pd.read_csv(filepath, header=None, sep=r"\s+")
 
 # Creating column names and sorting existing colums. 
 # Unit number, cycle and operation setting numb 1, 2 and 3. 
@@ -25,8 +26,8 @@ df = df.drop(columns = ["max_cycle"])
 
 #Set at threashold for 30 cycles remaining
 # if the RUL values is below 30 it will classify as 1 and flagged ass Failure_risk
-threashold = 30
-df["Failure_risk"] = np.where(df["RUL"] <= threashold, 1, 0)
+threshold = 30
+df["Failure_risk"] = np.where(df["RUL"] <= threshold, 1, 0)
 
 # checks whole cols and adding all operation_settings# and sensor# to feature_cols. Set these as X
 # y = column "Failure risk" 
@@ -47,7 +48,34 @@ model.fit(X_train, y_train)
 # Predicting with unseen data
 y_pred = model.predict(X_test)
 
-#Compare real value with predicted
-print(confusion_matrix(y_test, y_pred))
-print(classification_report(y_test, y_pred))
+X_all = df[feature_cols]
+
+df["pred_proba"] = model.predict_proba(X_all)[:, 1]
+#Reusing threashold variable for probability and same type of comparison as Failure risk and RUL
+threshold = 0.7
+df["pred_label"] = np.where(df["pred_proba"] > threshold, 1, 0)
+
+# Getting the latest cycle of each engine
+# Added and ofsett so we can look at earlier cycles than the last one and get a more interesting result
+ofsett = 30 # Amount of cyles before the last one. 
+latest = (df.sort_values("current_cycle")).groupby("unit_number").tail(1)
+latest_wofsett = (df.sort_values("current_cycle").groupby("unit_number")).nth(-ofsett)
+# Creating engine at risk
+engline_at_risk = latest_wofsett[latest_wofsett["pred_label"] == 1]
+
+plt.figure(figsize=(15, 5))
+
+colors = np.where(latest_wofsett["pred_label"] == 1, "red", "green")
+
+latest_wofsett_sorted = latest_wofsett.sort_values("unit_number")
+
+plt.bar(latest_wofsett_sorted["unit_number"].astype(str), latest_wofsett_sorted["pred_proba"], color=colors)
+plt.xticks(rotation=90)
+plt.ylabel("Predicted failure risk")
+plt.xlabel("Engine (unit_number)")
+plt.title("Predicted risk per engine")
+plt.tight_layout()
+
+plt.show()
+
 # %%
